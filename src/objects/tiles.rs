bb9ff779661859e22d0010302910154817f60211
jeffrey <jcouch210@gmail.com>
2025-11-27 13:13:54 -0900

    Rename tile data, entry, etc

--- src/objects/tiles.rs
+++ src/objects/tiles.rs
@@ -9,30 +9,30 @@ use crate::collections::{
 
 #[derive(Clone, Debug)]
 pub struct Tiles {
-    pub tiles: MultiTileGrid<TileEntry, TileShapeKey>,
+    pub tiles: MultiTileGrid<TileData, TileKey>,
 }
 
 #[derive(Clone, Debug)]
-pub struct TileEntry {}
+pub struct TileData {}
 
 new_key_type! {
-    pub struct TileShapeKey;
+    pub struct TileKey;
 }
 
-impl TileShape for TileShapeKey {
+impl TileShape for TileKey {
     fn offsets(&self) -> impl Iterator<Item = TileIndexOffset> {
-        TILE_SHAPES.entries[*self].offsets()
+        TILE_ENTRIES.entries[*self].offsets()
     }
 }
 
-pub static TILE_SHAPES: LazyLock<TileShapes> = LazyLock::new(TileShapes::default);
+pub static TILE_ENTRIES: LazyLock<TileEntries> = LazyLock::new(TileEntries::default);
 
 #[derive(Clone, Debug)]
-pub struct TileShapes {
-    pub entries: SlotMap<TileShapeKey, TileShapeEntry>,
+pub struct TileEntries {
+    pub entries: SlotMap<TileKey, TileEntry>,
 }
 
-impl Default for TileShapes {
+impl Default for TileEntries {
     fn default() -> Self {
         Self {
             entries: SlotMap::default(),
@@ -40,25 +40,25 @@ impl Default for TileShapes {
     }
 }
 
-impl TileShapes {
-    pub fn insert(&mut self, mut shape: TileShapeEntry) -> TileShapeKey {
-        shape.offsets.shrink_to_fit();
+impl TileEntries {
+    pub fn insert(&mut self, mut shape: TileEntry) -> TileKey {
+        shape.shape.shrink_to_fit();
 
         self.entries.insert(shape)
     }
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
-pub struct TileShapeEntry {
-    offsets: TileGrid<bool>,
+pub struct TileEntry {
+    shape: TileGrid<bool>,
 }
 
-impl TileShape for TileShapeEntry {
+impl TileShape for TileEntry {
     fn offsets(&self) -> impl Iterator<Item = TileIndexOffset> {
-        self.offsets.iter().map(|(offset, _)| offset.coords)
+        self.shape.iter().map(|(offset, _)| offset.coords)
     }
 
     fn bounds_hint(&self) -> Option<TileRect> {
-        Some(self.offsets.bounds())
+        Some(self.shape.bounds())
     }
 }
