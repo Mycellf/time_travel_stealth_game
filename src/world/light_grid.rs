9d106d8fb04ae5fca06564a6842b21d4322fb21d
jeffrey <jcouch210@gmail.com>
2025-11-29 13:02:27 -0900

    Add raycasting implementation

--- src/world/light_grid.rs
+++ src/world/light_grid.rs
@@ -1,12 +1,14 @@
 use std::mem;
 
-use nalgebra::{Point2, UnitVector2, Vector2};
+use nalgebra::{Point2, UnitVector2, Vector2, vector};
 
 use crate::collections::tile_grid::{TileGrid, TileIndex};
 
+type Tile = Option<MaterialKind>;
+
 #[derive(Clone, Debug)]
 pub struct LightGrid {
-    pub grid: TileGrid<Option<MaterialKind>>,
+    pub grid: TileGrid<Tile>,
     pub corners: TileGrid<Vec<Corner>>,
 }
 
@@ -106,4 +108,66 @@ pub struct LightArea {
 pub struct AngleRange {
     pub left: UnitVector2<f64>,
     pub right: UnitVector2<f64>,
+}
+
+impl LightGrid {
+    pub fn raycast_with(
+        &self,
+        mut function: impl FnMut(Point2<f64>, Tile) -> bool,
+        start: Point2<f64>,
+        direction: UnitVector2<f64>,
+    ) -> Point2<f64> {
+        let mut location = start;
+        let mut index = Self::index_of_location(location, direction.into_inner());
+
+        let dir_sign_x_int = if direction.x > 0.0 { 1 } else { -1 };
+        let dir_sign_y_int = if direction.y > 0.0 { 1 } else { -1 };
+
+        loop {
+            let time_x = (1.0 - (location.x * direction.x.signum()).rem_euclid(1.0)) / direction.x;
+            let time_y = (1.0 - (location.y * direction.y.signum()).rem_euclid(1.0)) / direction.y;
+
+            if time_x <= time_y {
+                Self::move_in_direction(&mut location.x, direction.x);
+            }
+
+            if time_x >= time_y {
+                Self::move_in_direction(&mut location.y, direction.y);
+            }
+
+            if time_x == time_y {
+                if function(location, self.grid[index + vector![dir_sign_x_int, 0]])
+                    || function(location, self.grid[index + vector![0, dir_sign_y_int]])
+                {
+                    break;
+                }
+            }
+
+            index = Self::index_of_location(location, direction.into_inner());
+            if function(location, self.grid[index]) {
+                break;
+            }
+        }
+
+        location
+    }
+
+    fn move_in_direction(location: &mut f64, direction: f64) {
+        if direction > 0.0 {
+            *location = location.floor() + 1.0;
+        } else {
+            *location = location.ceil() - 1.0;
+        }
+    }
+
+    fn index_of_location(location: Point2<f64>, direction: Vector2<f64>) -> Point2<isize> {
+        Vector2::from_fn(|i, _| {
+            (if direction[i] >= 0.0 {
+                location[i].floor()
+            } else {
+                location[i].ceil()
+            } as isize)
+        })
+        .into()
+    }
 }
