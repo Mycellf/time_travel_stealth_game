55c89da4d652cc5fc5566a457a6c1fe356d3e118
jeffrey <jcouch210@gmail.com>
2025-12-10 18:59:40 -0900

    Add button texture

--- src/level/entity_tracker/entity/button.rs
+++ src/level/entity_tracker/entity/button.rs
@@ -1,5 +1,9 @@
-use macroquad::math::Rect;
-use nalgebra::{Point2, Vector2, vector};
+use macroquad::{
+    color::colors,
+    math::Rect,
+    texture::{self, DrawTextureParams, Texture2D},
+};
+use nalgebra::{Point2, Vector2, point, vector};
 use serde::{Deserialize, Serialize};
 use slotmap::SlotMap;
 
@@ -17,9 +21,17 @@ use crate::{
 
 pub const BUTTON_SIZE: Vector2<f64> = vector![8.0, 8.0];
 
+pub const BUTTON_TEXTURE_POSITION: Point2<f32> = point![0.0, 48.0];
+pub const BUTTON_TEXTURE_SIZE: Vector2<f32> = vector![16.0, 16.0];
+
+pub const BUTTON_TOP_POSITION: Point2<f32> = point![4.0, 51.0];
+pub const BUTTON_TOP_SIZE: Vector2<f32> = vector![8.0, 8.0];
+
 #[derive(Clone, Serialize, Deserialize, Default, Debug)]
 pub struct Button {
     pub position: Point2<f64>,
+    #[serde(skip)]
+    pub pressed: bool,
 }
 
 impl Button {
@@ -45,6 +57,50 @@ impl Entity for Button {
         _initial_state: &mut SlotMap<EntityKey, EntityTracker>,
     ) -> Option<GameAction> {
         None
+    }
+
+    fn draw_floor(&mut self, texture_atlas: &Texture2D) {
+        texture::draw_texture_ex(
+            texture_atlas,
+            self.position.x as f32 - BUTTON_TEXTURE_SIZE.x / 2.0,
+            self.position.y as f32 - BUTTON_TEXTURE_SIZE.y / 2.0,
+            colors::WHITE,
+            DrawTextureParams {
+                source: Some(crate::new_texture_rect(
+                    BUTTON_TEXTURE_POSITION
+                        + vector![
+                            if self.pressed {
+                                BUTTON_TEXTURE_SIZE.x
+                            } else {
+                                0.0
+                            },
+                            0.0,
+                        ],
+                    BUTTON_TEXTURE_SIZE,
+                )),
+                ..Default::default()
+            },
+        );
+    }
+
+    fn draw_back(&mut self, texture_atlas: &Texture2D) {
+        if self.pressed {
+            return;
+        }
+
+        texture::draw_texture_ex(
+            texture_atlas,
+            self.position.x as f32 - BUTTON_TOP_SIZE.x / 2.0,
+            self.position.y as f32 - BUTTON_TOP_SIZE.y / 2.0 - 1.0,
+            colors::WHITE,
+            DrawTextureParams {
+                source: Some(crate::new_texture_rect(
+                    BUTTON_TOP_POSITION,
+                    BUTTON_TOP_SIZE,
+                )),
+                ..Default::default()
+            },
+        );
     }
 
     fn collision_rect(&self) -> Option<TileRect> {
@@ -74,11 +130,13 @@ impl Entity for Button {
     ) -> bool {
         let collision_rect = TileRect::from_rect_inclusive(self.collision_rect());
 
-        entities.iter().any(|(_, entity)| {
+        self.pressed = entities.iter().any(|(_, entity)| {
             entity
                 .inner
                 .collision_rect()
                 .is_some_and(|rect| rect.intersects(&collision_rect))
-        })
+        });
+
+        self.pressed
     }
 }
